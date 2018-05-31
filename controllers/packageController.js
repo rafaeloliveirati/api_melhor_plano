96f4d83b01befec2d82fade5acf60668b0c8ec3c
Rafael Oliveira <rafaeloliveira.ti@live.com>
2018-05-30 22:33:17 -0300

    dev un create new edge

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -1,71 +1,54 @@
 const PackageRepository = require('../repositories/packageRepository')();
+var plans = undefined
 
 exports.get = (req, res, next) => {
-    PackageRepository.findById('5b0d979a3056cc2d50b3b546').then((package) => {
-        var plans = [];
-        // packages.forEach(function (package) {
-        plans.push(package)
-        plans = createNewEdge(package, package.edges, plans);
-        // package.edges.forEach(function (packageEdge) {
-        //     if (!packageEdge)
-        //         return;
-        //     PackageRepository.findById(packageEdge._id).then((edge) => {
-        //         console.log(edge)
-        //         if (!edge)
-        //             return;
-        //         var plan = {
-        //             "name": package.name + " + " + edge.name,
-        //             "type": package.type + " + " + edge.type,
-        //             "value": Number(package.value) + Number(edge.value)
-        //         }
-        //         plans.push(plan)
-        //         edge.edges.forEach(function (edge) {
-        //             var plan = {
-        //                 "name": package.name + " + " + edge.name,
-        //                 "type": package.type + " + " + edge.type,
-        //                 "value": Number(package.value) + Number(edge.value)
-        //             }
-        //             plans.push(plan)
-        //         })
-        //     })
-        // })
-        // })
-        res.status(200).send(plans);
+    PackageRepository.findById('5b0d97ed3056cc2d50b3b548').then((package) => {
+        plans = []
+        // Create parent plan
+        createNewPlan(package)
+        //Create others plans by edges
+        createNewEdge(package, package.edges);
     }).catch(err => res.status(500).send(err))
+    res.status(200).send(plans);
 };
 
-createNewPlan = (package, edge, edge2) => {
+function createNewPlan(parent, edge) {
     var plan;
-    if (!edge2) {
+    if (!edge) {
         plan = {
-            "name": package.name + " + " + edge.name,
-            "type": package.type + " + " + edge.type,
-            "value": Number(package.value) + Number(edge.value)
+            "name": parent.name,
+            "type": parent.type,
+            "value": Number(parent.value)
         }
-        console.debug(plan);
     } else {
         plan = {
-            "name": package.name + " + " + edge.name + " + " + edge2.name,
-            "type": package.type + " + " + edge.type + " + " + edge2.type,
-            "value": Number(package.value) + Number(edge.value) + Number(edge2.value)
+            "name": parent.name + " + " + edge.name,
+            "type": parent.type + " + " + edge.type,
+            "value": Number(parent.value) + Number(edge.value)
         }
     }
+    plans.push(plan)
     return plan;
 }
 
-createNewEdge = (parent, edges, plans) => {
+function createNewEdge(parent, edges) {
     var plan;
-    for (i = 0; i < parent.edges.length; i++) {
-        plan = createNewPlan(parent, parent.edges[i]);
-        plans.push(plan)
-        if (!edges[i]._id) {
-            PackageRepository.findById(parent.edges[i]._id).then((edge) => {
-                createNewEdge(edge[i], edge, plans);
-            })
-        }
-
-        for (j = i + 1; j < parent.edges.length; j++) {
-            plans.push(createNewPlan(plan, parent.edges[i], parent.edges[j]))
+    if (!edges) {
+        plan = createNewPlan(parent);
+        return;
+    }
+    for (i = 0; i < edges.length; i++) {
+        var parentTemp = createNewPlan(parent, edges[i]);
+        PackageRepository.findById(edges[i]._id).then((package) => {
+            createNewEdge(parentTemp, package.edges)
+        })
+        for (j = i + 1; j < edges.length; j++) {
+            planTemp = {
+                "name": parentTemp.name + " + " + edges[j].name,
+                "type": parentTemp.type + " + " + edges[j].type,
+                "value": Number(parentTemp.value) + Number(edges[j].value)
+            }
+            createNewEdge(planTemp, edges[j].edges)
         }
     }
     return plans;
