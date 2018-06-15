115ca12ebe4743e65aefd293d0d873adb1b8f3f2
Rafael Oliveira <rafaeloliveira.ti@live.com>
2018-06-15 14:15:54 -0300

    enable coors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+var cors = require('cors')
+router.use(cors())
 
 router.get('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.status(200).send({
         title: "Melhor Plano 2018",
         version: "0.0.1"
--- routes/package.js
+++ routes/package.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const PackageController = require('../controllers/PackageController')
-
+var cors = require('cors')
+router.use(cors())
 
 router.get('/list-all-broadband', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     PackageController.get(req, res, next);
 });
 
